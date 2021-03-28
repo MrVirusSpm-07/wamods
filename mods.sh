a233f47a765b4de0954e8b858f91e531d8476cd8
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-03-28 13:39:44 +0700

    Add files via upload

---
+++ mods.sh
@@ -0,0 +1,332 @@
+BY MR•VIRUS-SPM
+#MY YT MR VIRUS SPM NEW
+#MY IG mvs.x_x
+
+                                                                                                     blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo                                                                                                 sleep 1
+echo $red
+echo "__        ___      ___ __  __ _   _ _   _ _____"
+echo "\ \      / / \    |_ _|  \/  | | | | \ | | ____|"
+echo " \ \ /\ / / _ \    | || |\/| | | | |  \| |  _|  "
+echo "  \ V  V / ___ \   | || |  | | |_| | |\  | |___ "
+echo "   \_/\_/_/   \_\ |___|_|  |_|\___/|_| \_|_____|"
+echo
+echo $blue"⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄⾄"
+echo
+echo $green"*****************************************"
+echo $yellow" Author "$blue"= "$red"Dicky Andre  "
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
+echo $green"*****************************************"
+echo
+echo
+sleep 1
+echo $red"SILAHKAN PILIH YANG MANA NGAB"
+echo $blue"++++++++++++++++++++++++++++++++++++"
+echo
+echo $yellow"1.) WA KEBAL ORIGINAL"
+echo $blue"+++++++++++++++++++++++++++++++++"
+echo $yellow"2.) WA IMUNE ANDROID 5-9"
+echo $blue"+++++++++++++++++++++++++++++++++"
+echo $yellow"3.) WA IMUNE ANDROID 10-12"
+echo $blue"+++++++++++++++++++++++++++++++++"
+echo $yellow"4.) WA IMUNE VERSI GB"
+echo $blue"+++++++++++++++++++++++++++++++++"
+echo $yellow"5.) WA KEBAL CLONE"
+echo $blue"+++++++++++++++++++++++++++++++++"
+echo $yellow"6.) YO WHATSAPP CLONE"
+echo $blue"+++++++++++++++++++++++++++++++++"
+echo $yellow"7.) YO WHATSAPP UNCLONE"
+echo $blue"+++++++++++++++++++++++++++++++++"
+echo $yellow"8.) WA IMUNE ALL ADMIN"
+echo $blue"+++++++++++++++++++++++++++++++++"
+echo $yellow"9.) WA IMUNE MELHOR"
+echo $blue"+++++++++++++++++++++++++++++++++"
+echo $yellow"10.) WA IMUNE BUSINES"
+echo $blue"+++++++++++++++++++++++++++++++++"
+echo $white
+sleep 1
+echo
+read -p "NOMOR BERAPA GAN : " dor
+
+if [ $dor = 1 ]
+then
+clear
+echo
+sleep 1
+echo $green"*****************************************"
+echo $yellow" Author "$blue"= "$red"Dicky Andre  "
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
+echo $green"*****************************************"
+echo
+echo
+sleep 1
+echo $red"  ___  ____  ___ ____ ___ _   _    _    _     "
+echo " / _ \|  _ \|_ _/ ___|_ _| \ | |  / \  | |    "
+echo "| | | | |_) || | |  _ | ||  \| | / _ \ | |    "
+echo "| |_| |  _ < | | |_| || || |\  |/ ___ \| |___ "
+echo " \___/|_| \_\___\____|___|_| \_/_/   \_\_____|"
+echo $green
+echo "AUTHOR   : Evan Exploit"
+echo "YOTUBE   : https://youtu.be/1Spw2N_4vxc"
+echo "WA KEBAL : https://www.mediafire.com/file/w3th9qs2fda0pzq/WhatsApp_Original7.apk/file"
+echo
+sleep 1
+echo $yellow"SILAHKAN SALIN LALU TEMPEL"
+echo "jangan lupa mampir ke yt yg tertara untuk mencari update"
+echo
+sleep 1
+fi
+if [ $dor = 2 ]                                            then
+clear
+echo                                                       sleep 1
+echo $green"*****************************************"
+echo $yellow" Author "$blue"= "$red"Dicky Andre  "
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"               echo $green"*****************************************"
+echo
+echo $red
+echo "__        ___      ___ __  __ _   _ _   _ _____"
+echo "\ \      / / \    |_ _|  \/  | | | | \ | | ____|"
+echo " \ \ /\ / / _ \    | || |\/| | | | |  \| |  _|  "
+echo "  \ V  V / ___ \   | || |  | | |_| | |\  | |___ "
+echo "   \_/\_/_/   \_\ |___|_|  |_|\___/|_| \_|_____|"
+echo $green
+echo "WHATSAP INI KHUSUS ANDROID 5 SAMPAI 9"
+echo $blue"++++++++++++++++++++++++++++++++++++++++++"
+echo
+echo $yellow                                               echo " AUTHOR   : HW MODS"                                 echo " CNL YT   : https://youtu.be/sHt2ePxGiuo"            echo " WA IMUNE : https://www.mediafire.com/file/hvg06q4v3obsvyx/for_android_9_-_5.apk/file"
+echo
+sleep 1
+echo $blue"
+echo "SILAHKAN TEMPEL LALU SALIN"
+echo                                                       echo "kunjungi cenel ytnya supaya melihat yg terbaru"
+echo
+sleep 1
+fi
+if [ $dor = 3 ]
+then
+echo 
+sleep 1
+echo $green"*****************************************"
+echo $yellow" Author "$blue"= "$red"Dicky Andre  "
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"               echo $green"*****************************************"
+echo
+echo $red
+echo "__        ___      ___ __  __ _   _ _   _ _____"
+echo "\ \      / / \    |_ _|  \/  | | | | \ | | ____|"
+echo " \ \ /\ / / _ \    | || |\/| | | | |  \| |  _|  "
+echo "  \ V  V / ___ \   | || |  | | |_| | |\  | |___ "
+echo "   \_/\_/_/   \_\ |___|_|  |_|\___/|_| \_|_____|"
+echo $green
+echo "WHATSAP INI KHUSUS ANDROID 10 SAMPAI 12"
+echo $blue"++++++++++++++++++++++++++++++++++++++++++"
+echo $yellow
+echo " AUTHOR    : HW MODS"
+echo " CNL YT    : https://youtu.be/sHt2ePxGiuo"
+echo " WA IMUNE  : https://www.mediafire.com/file/gfjqe1ip0if7phn/for_android_10.apk/file"
+echo
+echo
+sleep 1
+echo $blue"SILAHKAN SALIN LINK LALU TEMPELKAN DI CROM"
+echo
+echo "cek ke cnl yt untuk mengetahui yg terbaru"
+echo
+sleep 1
+fi
+if [ $dor = 4 ]
+then
+clear
+sleep 1
+echo $red                                                  echo "__        ___       ____ ____  "                     echo "\ \      / / \     / ___| __ ) "
+echo " \ \ /\ / / _ \   | |  _|  _ \ "
+echo "  \ V  V / ___ \  | |_| | |_) |"
+echo "   \_/\_/_/   \_\  \____|____/ "                     echo                                                       echo $green"*****************************************"
+echo $yellow" Author "$blue"= "$red"Dicky Andre  "
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
+echo $green"*****************************************"
+echo
+sleep 1
+echo $yellow
+echo " Author   : AmziModer"
+echo " Cnl Yt   : https://youtu.be/UdaiRnCHGWk"
+echo " Wa Imune : https://www.mediafire.com/file/z0kpvumagx645jk/GB_BY_%25E3%2581%25BC%25E2%2588%2586ZMIMODS%25E4%25B9%2588.apk/file"
+echo
+sleep 1
+echo $blue
+echo "SILAHKAN SALIN LINK TEMPEL KE GOOGLE CROM"
+echo
+echo "cek cnl yt untuk melihat yg terbaru"
+echo
+sleep 1
+fi
+if [ $dor = 5 ]                                                                                      then
+clear                                                                                                sleep 1
+echo $purple
+echo "__        ___       ____ _     ___  _   _ _____"
+echo "\ \      / / \     / ___| |   / _ \| \ | | ____|"
+echo " \ \ /\ / / _ \   | |   | |  | | | |  \| |  _|  "
+echo "  \ V  V / ___ \  | |___| |__| |_| | |\  | |___ "                                              echo "   \_/\_/_/   \_\  \____|_____\___/|_| \_|_____|"
+echo
+echo
+echo $green"*****************************************"
+echo $yellow" Author "$blue"= "$red"Dicky Andre"
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
+echo $green"*****************************************"
+echo
+sleep 1
+echo $yellow
+echo " Author   : Firman Enjoy"
+echo " Link Yt  : https://youtu.be/8MqCI_6aXAQ"
+echo " Link Wa  : https://www.mediafire.com/file/ayd1de8kjin0gtb/WhatsApp_ori_clone.apk/file"
+echo
+sleep 1
+echo $blue
+echo "SILAGKAN SALIN LINK TEMPEL KE GOOGLE CROM"
+echo
+echo "cek link yt untuk melihat versi terbaru"
+echo
+sleep 1
+echo
+fi
+if [ $dor = 6 ]
+then
+clear
+echo
+sleep 1
+echo
+echo $purple
+echo "__        ___       ____ _     ___  _   _ _____"
+echo "\ \      / / \     / ___| |   / _ \| \ | | ____|"
+echo " \ \ /\ / / _ \   | |   | |  | | | |  \| |  _|  "
+echo "  \ V  V / ___ \  | |___| |__| |_| | |\  | |___ "
+echo "   \_/\_/_/   \_\  \____|_____\___/|_| \_|_____|"
+echo
+sleep 1
+echo
+echo $green"*****************************************"
+echo $yellow" Author "$blue"= "$red"Dicky Andre"
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
+echo $green"*****************************************"
+echo
+echo $white
+echo " Author   : He Petrik"
+echo " Link yt  : https://youtu.be/JwcfEG3Z_7I"
+echo " Link Wa  : https://www.mediafire.com/file/fyxex28l2nu43yw/YOWA8.71_By-SamMods.zip/file"
+echo
+echo $green
+sleep 1
+echo "EKSTRAK DULU GENG PASSWORD :" $white"yoowa"
+echo
+echo
+sleep 1
+fi
+if [ $dor = 7 ]
+then
+clear
+sleep 1
+echo
+echo $red
+echo "__        ___      _   _ _   _  ____ _     ___  _   _ _____"
+echo "\ \      / / \    | | | | \ | |/ ___| |   / _ \| \ | | ____|"
+echo " \ \ /\ / / _ \   | | | |  \| | |   | |  | | | |  \| |  _|  "
+echo "  \ V  V / ___ \  | |_| | |\  | |___| |__| |_| | |\  | |___ "
+echo "   \_/\_/_/   \_\  \___/|_| \_|\____|_____\___/|_| \_|_____|"
+echo
+sleep 1
+echo
+echo $green"*****************************************"
+echo $yellow" Author "$blue"= "$red"Dicky Andre"
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
+echo $green"*****************************************"
+echo $purple
+echo " Author   : He Petrik"
+echo " Cnl Yt   : https://youtu.be/JwcfEG3Z_7I"
+echo " Link Wa  : https://www.mediafire.com/file/08mq6wcg1kjj3r9/YOWhats%252B_8.71_By-SamMods.zip/file"
+echo
+sleep 1
+echo $white
+echo "EKSTRAK DULU GENG PASWORD :" $yellow"yoowa"
+echo
+sleep 1
+fi
+fi
+if [ $dor = 8 ]
+then
+clear
+sleep 1
+echo $yellow
+echo "    _    _     _          _    ____  __  __ ___ _   _ "          echo "   / \  | |   | |        / \  |  _ \|  \/  |_ _| \ | |"
+echo "  / _ \ | |   | |       / _ \ | | | | |\/| || ||  \| |"          echo " / ___ \| |___| |___   / ___ \| |_| | |  | || || |\  |"
+echo "/_/   \_\_____|_____| /_/   \_\____/|_|  |_|___|_| \_|"
+echo                                                                   echo                                                                   sleep 1
+echo
+echo $green"*****************************************"
+echo $yellow" Author "$blue"= "$red"Dicky Andre"
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"                  echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"                           echo $green"*****************************************"
+echo $purple
+echo
+echo " Author    : Xdinz Channel"                                      echo " Link Yt   : https://youtu.be/XP0leEOuUY8"
+echo " Link Wa   : https://www.mediafire.com/file/kghksi95o08a0qs/%25E2%259C%25BF%25CD%259C%25CD%25A1%25E0%25A6%2594%25E0%25A7%25A3X%25CD%259E%25CD%259F%25CC%25B8d%25CD%259E%25CD%259F%25CC%25B8i%25CD%259E%25CD%259F%25CC%25B8n%25CD%259E%25CD%259F%25CC%25B8z%25CD%259E%25CD%259F%25CC%25B8_M%25CD%259E%25CD%259F%25CC%25B8o%25CD%259E%25CD%259F%25CC%25B8d%25CD%259E%25CD%259F%25CC%25B8s%25CD%259E%25CD%259F%25CC%25B8-W%25CD%259E%25CD%259F%25CC%25B8A%25CD%259E%25CD%259F%25CC%25B8%25CD%259E%25CD%259F%25CC%25B8%25E2%259C%25AA%25CD%25A1%25CD%259C%25E2%259E%25A2_2.20.202.18.apk/file"
+echo
+echo $blue                                                             echo "TINGGAL SALIN LINK TEMPEL KE CROOM"
+echo                                                                   echo                                                                   sleep 1
+fi
+if [ $dor = 9 ]
+then
+clear
+sleep 1
+echo
+echo " __  __ _____ _     _   _  ___  ____   "
+echo "|  \/  | ____| |   | | | |/ _ \|  _ \  "
+echo "| |\/| |  _| | |   | |_| | | | | |_) | "                         echo "| |  | | |___| |___|  _  | |_| |  _ <  "                         echo "|_|  |_|_____|_____|_| |_|\___/|_| \_\ "
+echo
+echo
+sleep 1                                                                echo
+echo $green"*****************************************"
+echo $yellow" Author "$blue"= "$red"Dicky Andre"
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"                  echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
+echo $green"*****************************************"
+echo $white                                                            echo " Author    : PRUEBA"
+echo " Link Yt   : https://youtu.be/qD9IImkXcPQ"
+echo " Link Wa   : https://www.mediafire.com/file/1ko51ldngaswnum/%25EA%25A7%2581%25E2%259E%25A5%25E2%2583%25A2%25E2%259C%259F%25F0%2592%2591%2589%25E2%2598%259E%25E2%259D%25AE%25F0%259D%2590%258D%25F0%259D%2590%25A4%25F0%259D%2590%258C%25F0%259D%2590%25A8%25F0%259D%2590%259D%25F0%259D%2590%25AC_%25F0%259D%2590%2593%25F0%259D%2590%259A%25F0%259D%2590%25A0%25F0%259D%2590%259A%25F0%259D%2590%25A5%25F0%259D%2590%25A5%25F0%259D%259F%2590%25F0%259D%259F%2592_%25E2%259D%25AF%25E2%2598%259C%25E2%259B%25A7%25E2%2583%25A2%25F0%2593%2583%25B5%25EA%25A7%2582_.apk/file"
+echo
+echo $red
+sleep 1
+echo "SILAHKAN SALIN LALU TEMPEL DI CROOM"
+echo
+sleep 1
+fi
+if [ $dor = 10 ]
+then
+clear                                                                  sleep 1                                                                echo $blue
+echo " ____  _   _ ____ ___ _   _ _____ ____  "
+echo "| __ )| | | / ___|_ _| \ | | ____/ ___| "
+echo "|  _ \| | | \___ \| ||  \| |  _| \___ \ "                        echo "| |_) | |_| |___) | || |\  | |___ ___) |"
+echo "|____/ \___/|____/___|_| \_|_____|____/ "
+echo
+sleep 1
+echo $green
+echo " Author   : Hw Mods"
+echo " Link Yt  : https://youtu.be/tdq7vbbVrw0"
+echo " Link Wa  : https://www.mediafire.com/file/zymmypfn32n1htp/WhatsApp_Business_2.21.2.18.apk/file"
+echo
+echo $white
+sleep 1
+echo "SILAHKAN DI COPY TEMPEL DI CROOM"                                echo                                                                   echo
+sleep 1
+fi
