6ce72a9d0eae9905f1f4f1d64b5afb3faa44ef2f
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-03-28 13:48:54 +0700

    Update mods.sh

--- mods.sh
+++ mods.sh
@@ -1,8 +1,7 @@
 BY MR•VIRUS-SPM
 #MY YT MR VIRUS SPM NEW
 #MY IG mvs.x_x
-
-                                                                                                     blue='\033[34;1m'
+blue='\033[34;1m'
 green='\033[32;1m'
 purple='\033[35;1m'
 cyan='\033[36;1m'
@@ -11,7 +10,8 @@ white='\033[37;1m'
 yellow='\033[33;1m'
 
 clear
-echo                                                                                                 sleep 1
+echo
+sleep 1                                                                                                 
 echo $red
 echo "__        ___      ___ __  __ _   _ _   _ _____"
 echo "\ \      / / \    |_ _|  \/  | | | | \ | | ____|"
@@ -86,13 +86,16 @@ echo "jangan lupa mampir ke yt yg tertara untuk mencari update"
 echo
 sleep 1
 fi
-if [ $dor = 2 ]                                            then
+if [ $dor = 2 ]
+then
 clear
-echo                                                       sleep 1
+echo
+sleep 1
 echo $green"*****************************************"
 echo $yellow" Author "$blue"= "$red"Dicky Andre  "
 echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
-echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"               echo $green"*****************************************"
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
+echo $green"*****************************************"
 echo
 echo $red
 echo "__        ___      ___ __  __ _   _ _   _ _____"
@@ -104,12 +107,16 @@ echo $green
 echo "WHATSAP INI KHUSUS ANDROID 5 SAMPAI 9"
 echo $blue"++++++++++++++++++++++++++++++++++++++++++"
 echo
-echo $yellow                                               echo " AUTHOR   : HW MODS"                                 echo " CNL YT   : https://youtu.be/sHt2ePxGiuo"            echo " WA IMUNE : https://www.mediafire.com/file/hvg06q4v3obsvyx/for_android_9_-_5.apk/file"
+echo $yellow
+echo " AUTHOR   : HW MODS"                                 
+echo " CNL YT   : https://youtu.be/sHt2ePxGiuo"            
+echo " WA IMUNE : https://www.mediafire.com/file/hvg06q4v3obsvyx/for_android_9_-_5.apk/file"
 echo
 sleep 1
 echo $blue"
 echo "SILAHKAN TEMPEL LALU SALIN"
-echo                                                       echo "kunjungi cenel ytnya supaya melihat yg terbaru"
+echo                                                       
+echo "kunjungi cenel ytnya supaya melihat yg terbaru"
 echo
 sleep 1
 fi
@@ -120,7 +127,8 @@ sleep 1
 echo $green"*****************************************"
 echo $yellow" Author "$blue"= "$red"Dicky Andre  "
 echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
-echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"               echo $green"*****************************************"
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"               
+echo $green"*****************************************"
 echo
 echo $red
 echo "__        ___      ___ __  __ _   _ _   _ _____"
@@ -148,10 +156,14 @@ if [ $dor = 4 ]
 then
 clear
 sleep 1
-echo $red                                                  echo "__        ___       ____ ____  "                     echo "\ \      / / \     / ___| __ ) "
+echo $red                                                  
+echo "__        ___       ____ ____  "                    
+ echo "\ \      / / \     / ___| __ ) "
 echo " \ \ /\ / / _ \   | |  _|  _ \ "
 echo "  \ V  V / ___ \  | |_| | |_) |"
-echo "   \_/\_/_/   \_\  \____|____/ "                     echo                                                       echo $green"*****************************************"
+echo "   \_/\_/_/   \_\  \____|____/ "                     
+echo                                                       
+echo $green"*****************************************"
 echo $yellow" Author "$blue"= "$red"Dicky Andre  "
 echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"
 echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
@@ -171,13 +183,16 @@ echo "cek cnl yt untuk melihat yg terbaru"
 echo
 sleep 1
 fi
-if [ $dor = 5 ]                                                                                      then
-clear                                                                                                sleep 1
+if [ $dor = 5 ]                                                                                      
+then
+clear                                                                                                
+sleep 1
 echo $purple
 echo "__        ___       ____ _     ___  _   _ _____"
 echo "\ \      / / \     / ___| |   / _ \| \ | | ____|"
 echo " \ \ /\ / / _ \   | |   | |  | | | |  \| |  _|  "
-echo "  \ V  V / ___ \  | |___| |__| |_| | |\  | |___ "                                              echo "   \_/\_/_/   \_\  \____|_____\___/|_| \_|_____|"
+echo "  \ V  V / ___ \  | |___| |__| |_| | |\  | |___ "                                              
+echo "   \_/\_/_/   \_\  \____|_____\___/|_| \_|_____|"
 echo
 echo
 echo $green"*****************************************"
@@ -270,21 +285,31 @@ then
 clear
 sleep 1
 echo $yellow
-echo "    _    _     _          _    ____  __  __ ___ _   _ "          echo "   / \  | |   | |        / \  |  _ \|  \/  |_ _| \ | |"
-echo "  / _ \ | |   | |       / _ \ | | | | |\/| || ||  \| |"          echo " / ___ \| |___| |___   / ___ \| |_| | |  | || || |\  |"
+echo "    _    _     _          _    ____  __  __ ___ _   _ "          
+echo "   / \  | |   | |        / \  |  _ \|  \/  |_ _| \ | |"
+echo "  / _ \ | |   | |       / _ \ | | | | |\/| || ||  \| |"          
+echo " / ___ \| |___| |___   / ___ \| |_| | |  | || || |\  |"
 echo "/_/   \_\_____|_____| /_/   \_\____/|_|  |_|___|_| \_|"
-echo                                                                   echo                                                                   sleep 1
+echo                                                                   
+echo                                                                   
+sleep 1
 echo
 echo $green"*****************************************"
 echo $yellow" Author "$blue"= "$red"Dicky Andre"
-echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"                  echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"                           echo $green"*****************************************"
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"                  
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"                           
+echo $green"*****************************************"
 echo $purple
 echo
-echo " Author    : Xdinz Channel"                                      echo " Link Yt   : https://youtu.be/XP0leEOuUY8"
+echo " Author    : Xdinz Channel"                                      
+echo " Link Yt   : https://youtu.be/XP0leEOuUY8"
 echo " Link Wa   : https://www.mediafire.com/file/kghksi95o08a0qs/%25E2%259C%25BF%25CD%259C%25CD%25A1%25E0%25A6%2594%25E0%25A7%25A3X%25CD%259E%25CD%259F%25CC%25B8d%25CD%259E%25CD%259F%25CC%25B8i%25CD%259E%25CD%259F%25CC%25B8n%25CD%259E%25CD%259F%25CC%25B8z%25CD%259E%25CD%259F%25CC%25B8_M%25CD%259E%25CD%259F%25CC%25B8o%25CD%259E%25CD%259F%25CC%25B8d%25CD%259E%25CD%259F%25CC%25B8s%25CD%259E%25CD%259F%25CC%25B8-W%25CD%259E%25CD%259F%25CC%25B8A%25CD%259E%25CD%259F%25CC%25B8%25CD%259E%25CD%259F%25CC%25B8%25E2%259C%25AA%25CD%25A1%25CD%259C%25E2%259E%25A2_2.20.202.18.apk/file"
 echo
-echo $blue                                                             echo "TINGGAL SALIN LINK TEMPEL KE CROOM"
-echo                                                                   echo                                                                   sleep 1
+echo $blue                                                            
+echo "TINGGAL SALIN LINK TEMPEL KE CROOM"
+echo                                                                   
+echo                                                                   
+sleep 1
 fi
 if [ $dor = 9 ]
 then
@@ -293,15 +318,20 @@ sleep 1
 echo
 echo " __  __ _____ _     _   _  ___  ____   "
 echo "|  \/  | ____| |   | | | |/ _ \|  _ \  "
-echo "| |\/| |  _| | |   | |_| | | | | |_) | "                         echo "| |  | | |___| |___|  _  | |_| |  _ <  "                         echo "|_|  |_|_____|_____|_| |_|\___/|_| \_\ "
+echo "| |\/| |  _| | |   | |_| | | | | |_) | "                         
+echo "| |  | | |___| |___|  _  | |_| |  _ <  "                         
+echo "|_|  |_|_____|_____|_| |_|\___/|_| \_\ "
 echo
 echo
-sleep 1                                                                echo
+sleep 1                                                               
+echo
 echo $green"*****************************************"
 echo $yellow" Author "$blue"= "$red"Dicky Andre"
-echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"                  echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
+echo $yellow" Cnl Yt "$blue"= "$red"MR VIRUS SPM NEW"                  
+echo $yellow" Nm Ig  "$blue"= "$red"mvs.x_x"
 echo $green"*****************************************"
-echo $white                                                            echo " Author    : PRUEBA"
+echo $white                                                            
+echo " Author    : PRUEBA"
 echo " Link Yt   : https://youtu.be/qD9IImkXcPQ"
 echo " Link Wa   : https://www.mediafire.com/file/1ko51ldngaswnum/%25EA%25A7%2581%25E2%259E%25A5%25E2%2583%25A2%25E2%259C%259F%25F0%2592%2591%2589%25E2%2598%259E%25E2%259D%25AE%25F0%259D%2590%258D%25F0%259D%2590%25A4%25F0%259D%2590%258C%25F0%259D%2590%25A8%25F0%259D%2590%259D%25F0%259D%2590%25AC_%25F0%259D%2590%2593%25F0%259D%2590%259A%25F0%259D%2590%25A0%25F0%259D%2590%259A%25F0%259D%2590%25A5%25F0%259D%2590%25A5%25F0%259D%259F%2590%25F0%259D%259F%2592_%25E2%259D%25AF%25E2%2598%259C%25E2%259B%25A7%25E2%2583%25A2%25F0%2593%2583%25B5%25EA%25A7%2582_.apk/file"
 echo
@@ -313,10 +343,13 @@ sleep 1
 fi
 if [ $dor = 10 ]
 then
-clear                                                                  sleep 1                                                                echo $blue
+clear                                                                  
+sleep 1                                                                
+echo $blue
 echo " ____  _   _ ____ ___ _   _ _____ ____  "
 echo "| __ )| | | / ___|_ _| \ | | ____/ ___| "
-echo "|  _ \| | | \___ \| ||  \| |  _| \___ \ "                        echo "| |_) | |_| |___) | || |\  | |___ ___) |"
+echo "|  _ \| | | \___ \| ||  \| |  _| \___ \ "                        
+echo "| |_) | |_| |___) | || |\  | |___ ___) |"
 echo "|____/ \___/|____/___|_| \_|_____|____/ "
 echo
 sleep 1
@@ -327,6 +360,8 @@ echo " Link Wa  : https://www.mediafire.com/file/zymmypfn32n1htp/WhatsApp_Busine
 echo
 echo $white
 sleep 1
-echo "SILAHKAN DI COPY TEMPEL DI CROOM"                                echo                                                                   echo
+echo "SILAHKAN DI COPY TEMPEL DI CROOM"                                
+echo                                                                   
+echo
 sleep 1
 fi
